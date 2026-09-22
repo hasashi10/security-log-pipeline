235834638dc9f2d6df317c0fe324f8ad2f2c7d30
hasashi10 <hanz15@live.com>
2026-09-22 09:19:52 -0400

    remove timestamp test line

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
-#include <fstream>
+#include<fstream>
 #include<iostream>
 #include<string>
 #include<vector>
 #include<unordered_map>
+#include<ctime>
+#include<sstream>
+#include<iomanip>
 
 
+
+std::time_t parseTimestamp(const std::string& raw){
+    std::tm tm{};
+    std::istringstream ss(raw);
+    ss>> std::get_time(&tm, "%b %d %H:%M:%S");
+    tm.tm_year = 126;
+    return std::mktime(&tm);
+}
 enum class EventType{ AuthFailure, SudoSuccess, Other};
 
 
 class LogEvent{
     public:
-        LogEvent(const std::string& timestamp, const std::string&user, EventType type):
-            timestamp_(timestamp), user_(user), type_(type){}
+        LogEvent(const std::string& timestamp, const std::string&user, EventType type, std::time_t time):
+            timestamp_(timestamp), user_(user), type_(type), time_(time){}
 
         const std::string& timestamp() const { return timestamp_;}
         const std::string& user() const{return user_;}
         EventType type() const{ return type_;}
+        std::time_t time() const {return time_;}
     
     private:
         std::string timestamp_;
         std::string user_;
         EventType type_;
+        std::time_t time_;
 
 
 };
@@ -38,9 +51,10 @@ LogEvent parseLine(const std::string& line){
         }
     } else if (line.find("COMMAND=") != std::string::npos &&
                line.find("incorrect password attempts") == std::string::npos){
-                type = EventType::SudoSuccess;
-               }
-               return LogEvent(timestamp, user, type);
+        type = EventType::SudoSuccess;
+    }
+    std::time_t time = parseTimestamp(timestamp);
+    return LogEvent(timestamp, user, type, time);
 }
 
 int main(){
@@ -48,13 +62,14 @@ int main(){
     if(!file){
         std::cerr<<"could not opent data/sudo_events.txt\n";
         return 1;
-    }    
-   std::vector<LogEvent> events;
+    }   
+    
+    std::vector<LogEvent> events;
     std::string line;
     while (std::getline(file, line)){
         events.push_back(parseLine(line));
     }
-    std::cout<<"Parsed" <<events.size() << "events\n";
+    std::cout<< "Parsed " <<events.size() << " events\n";
     std::unordered_map<std::string, int> failureByUser;
 
     int failures = 0;
@@ -69,7 +84,7 @@ int main(){
         }
     }
     for (const auto& pair : failureByUser){
-        std::cout<< pair.first<<": "<<"faulures\n";
+        std::cout<< pair.first<<": "<< pair.second << " failures\n";
     }
     std::cout<<"failures: "<<failures <<"\n";
     std::cout<<"successes: "<<successes<<"\n";
